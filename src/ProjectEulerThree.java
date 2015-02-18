106ee7acc900980e38ff398e6b517ebce726c658
Anupriya Srivastava <anupriya@hawaii.edu>
2015-02-17 21:44:25 -1000

    fixed

--- src/ProjectEulerThree.java
+++ src/ProjectEulerThree.java
@@ -5,30 +5,30 @@ import java.util.ArrayList;
  */
 public class ProjectEulerThree {
   /**
-   * Calcultes the largest prime factor of the number 600851475143.
+   * Calculates the largest prime factor of the number 600851475143.
    * @param args Ignored.
    */
-  public  static void main(String[] args){
-    long number = 600851475143l;
-    ArrayList<Long> pf = calculatesLargestPrimeFactor(number);
-    System.out.print("largest factor"+pf.get(pf.size()-1));
+  public  static void main(String[] args) {
+    long number = 600851475143L , largestFactor;
+    largestFactor = calculatesLargestPrimeFactor(number);
+    System.out.print("largest factor = " + largestFactor);
   }
 
   /**
-   * Calcultes the largest prime factor of the number.
+   * Calculates the largest prime factor of the number.
    * @param number The number who's largest prime factor is to be calculated.
-   * @return the ArrayList containg the largest prime factor.
+   * @return the ArrayList containing the largest prime factor.
    */
-  public static ArrayList<Long> calculatesLargestPrimeFactor(long number) {
+  public static long calculatesLargestPrimeFactor(long number) {
     ArrayList<Long> pf = new ArrayList<Long>();
-    long num = number, i = 2l;
-    while (num>1) {
+    long num = number, i = 2L;
+    while (num > 1) {
       while (num % i == 0) {
         pf.add(i);
         num /= i;
       }
       i++;
     }
-    return pf;
+    return pf.get(pf.size() - 1);
   }
 }
